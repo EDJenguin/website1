ea319aee6b3eda72994d6178abc3a7ce0ad6509b
EDJenguin <edwinjoyce.inbox+ghub@gmail.com>
2026-04-14 19:35:09 +0000

    added images

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,32 @@
+import io
+
 import streamlit as st
+from PIL import Image
 
-print('ran')
+upload = st.file_uploader('Please upload a png file', type='png', accept_multiple_files=False)
 
-st.write('hello there')
+if upload is not None:
+    image = Image.open(upload)
+    st.image(image, caption='Uploaded image')
+
+    angle = st.slider(
+        'Rotate image by degrees',
+        min_value=0,
+        max_value=360,
+        value=0,
+        step=45,
+    )
+    rotated_image = image.rotate(angle, expand=True)
+
+    st.image(rotated_image, caption=f'Rotated image ({angle}°)')
+
+    buffer = io.BytesIO()
+    rotated_image.save(buffer, format='PNG')
+    buffer.seek(0)
+
+    st.download_button(
+        'Download rotated image',
+        data=buffer,
+        file_name='rotated.png',
+        mime='image/png',
+    )
